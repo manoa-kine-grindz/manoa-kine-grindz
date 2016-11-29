a3b76b0ff789701e9fa5df9d9495f23c3a538435
kpk3 <kpk3@hawaii.edu>
2016-11-29 04:29:04 -1000

    Finished Mockup for Search For Page

--- app/imports/startup/server/vendorMenu.js
+++ app/imports/startup/server/vendorMenu.js
@@ -1,7 +1,19 @@
 /**
  * Created by mhoe1 on 11/10/2016.
  */
-import { Vendors } from '../../api/vendors/vendors.js';
-import { MenuItems } from '../../api/menuItems/menuItems.js';
-import { _ } from 'meteor/underscore';
+import {Vendors} from '../../api/vendors/vendors.js';
+import {MenuItems} from '../../api/menuItems/menuItems.js';
+import {_} from 'meteor/underscore';
 
+const vendorSeeds = [
+  { name: 'Panda Express', cuisine: 'Chinese' },
+  { name: 'Subway', cuisine: 'Subway Sandwhiches' },
+  { name: 'Da Grill', cuisine: 'Local Hawaiian' },
+  { name: 'Stir Fresh', cuisine: 'Flash Fried' },
+];
+
+if (Vendors.find().count() === 0) {
+  _.each(vendorSeeds, function seedVendors(stuff) {
+    Vendors.insert(stuff);
+  });
+}
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -5,3 +5,5 @@ import './menu.js';
 import './calendar.html';
 import './calendar.js';
 
+import './search-for.html';
+import './search-for.js';
